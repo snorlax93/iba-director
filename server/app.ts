17a1906c1501b4b464eaf998344549eb7d9cb9f7
LA <teamhierro@Mac.attlocal.net>
2024-10-27 05:18:49 -0400

    moving code to backend to start frontend

--- server/app.ts
+++ server/app.ts
@@ -7,10 +7,6 @@ const app = new Hono()
 
 app.use('*', logger());
 
-app.get('/test', (c) => {
-    return c.json({ 'message': 'Hello Hono!' })
-})
-
 app.route('/api/topics', topicsRoute);
 app.route('/api/episodes', episodesRoute);
 
